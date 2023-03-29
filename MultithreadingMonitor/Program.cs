88514308abea3892e0a011638736fddfc60cbccf
nehabirajdar <nsb9823@gmail.com>
2023-03-29 12:56:21 +0530

    Add project files.

---
+++ MultithreadingMonitor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingMonitor
+{
+    public class Tasks
+    {
+        private object _lock = new object();
+
+        public void Display1To5()
+        {
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine(thread.Name);
+            Monitor.Enter(_lock);
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Monitor.Exit(_lock);
+
+        }
+        public void Display6To10()
+        {
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine(thread.Name);
+            for (int i = 6; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks task1 = new Tasks();
+            Thread t1 = new Thread(new ThreadStart(task1.Display1To5));
+            Thread t2 = new Thread(new ThreadStart(task1.Display1To5));
+            Thread t3 = new Thread(new ThreadStart(task1.Display1To5));
+            t1.Name = "Display1-5";
+            t2.Name = "Display6-10";
+            t3.Name = "Thread 3";
+            //t1.Priority = ThreadPriority.Lowest;
+            //t2.Priority = ThreadPriority.Highest;
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
